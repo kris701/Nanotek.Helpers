aabb327994ba6f9f881e33add15defc7afcf40e0
Kristian Skov <kris701kj@gmail.com>
2022-12-01 18:31:24 +0100

    Added initial helper

--- Nanotek.Helpers/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Nanotek.Helpers
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Nanotek.Helpers/DirectoryHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Nanotek.Helpers
+{
+    public static class DirectoryHelper
+    {
+        public static string CombinePathAndFile(string path, string file)
+        {
+            path = path.Trim();
+            file = file.Trim();
+            if (!path.EndsWith("\\"))
+                path = $"{path}\\";
+            if (file.StartsWith("\\"))
+                file = file.Substring(1);
+            return $"{path}{file}";
+        }
+
+        public static string CombinePathAndFile(DirectoryInfo path, FileInfo file)
+        {
+            return CombinePathAndFile(path.FullName, file.Name);
+        }
+    }
+}
