002e97a6464c48e4941117a4817fb4781b643c88
Kristian Skov <kris701kj@gmail.com>
2023-02-23 20:25:11 +0100

    Added a dictionary helper

---
+++ Nanotek.Helpers/DictionaryHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanotek.Helpers
+{
+    public static class DictionaryHelper
+    {
+        public static void AddOrIncrement<T>(Dictionary<T, int> dict, T key, int by = 1) where T : notnull
+        {
+            if (dict.ContainsKey(key))
+                dict[key] += by;
+            else
+                dict.Add(key, by);
+        }
+
+        public static void AddOrIncrement<T>(Dictionary<T, double> dict, T key, double by = 1) where T : notnull
+        {
+            if (dict.ContainsKey(key))
+                dict[key] += by;
+            else
+                dict.Add(key, by);
+        }
+
+        public static void AddOrIncrement<T>(Dictionary<T, long> dict, T key, long by = 1) where T : notnull
+        {
+            if (dict.ContainsKey(key))
+                dict[key] += by;
+            else
+                dict.Add(key, by);
+        }
+    }
+}
