93d5bf6ce63f14506e5424883257a7ef8dc8544d
Kristian Skov <kris701kj@gmail.com>
2023-03-01 11:45:09 +0100

    Added an initial math helper, with tests as well

---
+++ Nanotek.Helpers.Tests/MathHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanotek.Helpers.Tests
+{
+    [TestClass]
+    public class MathHelperTests
+    {
+        #region EuclideanDistance
+
+        [TestMethod]
+        [DataRow(1,2,1)]
+        [DataRow(1,10,9)]
+        [DataRow(1,-5,6)]
+        public void EuclideanDistance1D_Is_Correct(double v1, double v2, double expected)
+        {
+            Assert.AreEqual(expected, MathHelper.EuclideanDistance1D(v1,v2));
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 10, 10, 14)]
+        [DataRow(-50, 10, 532, 10, 582)]
+        public void EuclideanDistance2D_Is_Correct(double x1, double y1, double x2, double y2, double expected)
+        {
+            Assert.AreEqual(expected, Math.Floor(MathHelper.EuclideanDistance2D(x1, y1, x2, y2)));
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 10, 10, 14)]
+        [DataRow(-50, 10, 532, 10, 582)]
+        public void EuclideanDistance2D_Points_Is_Correct(int x1, int y1, int x2, int y2, double expected)
+        {
+            Assert.AreEqual(expected, Math.Floor(MathHelper.EuclideanDistance2D(new Point(x1, y1), new Point(x2, y2))));
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 0, 10, 10, 10, 17)]
+        [DataRow(-50, 10, 61, 532, 10, -52, 592)]
+        public void EuclideanDistance3D_Is_Correct(double x1, double y1, double z1, double x2, double y2, double z2, double expected)
+        {
+            Assert.AreEqual(expected, Math.Floor(MathHelper.EuclideanDistance3D(x1, y1, z1, x2, y2, z2)));
+        }
+
+        #endregion
+    }
+}
---
+++ Nanotek.Helpers/MathHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanotek.Helpers
+{
+    /// <summary>
+    /// A helper class with various different mathematical helper methods
+    /// </summary>
+    public static class MathHelper
+    {
+        #region EuclideanDistance
+
+        /// <summary>
+        /// Get the Euclidean distance between two points in 1D space
+        /// </summary>
+        /// <remarks><see href="https://en.wikipedia.org/wiki/Euclidean_distance"/></remarks>
+        /// <returns>The Euclidean distance between two values</returns>
+        public static double EuclideanDistance1D(double v1, double v2)
+        {
+            return Math.Sqrt(Math.Pow(v2 - v1, 2));
+        }
+
+        /// <summary>
+        /// Get the Euclidean distance between two <seealso cref="Point"/>s in 2D space.
+        /// </summary>
+        /// <remarks><see href="https://en.wikipedia.org/wiki/Euclidean_distance"/></remarks>
+        /// <returns>The Euclidean distance between <paramref name="a"/> and <paramref name="b"/></returns>
+        public static double EuclideanDistance2D(Point a, Point b) => EuclideanDistance2D(a.X, a.Y, b.X, b.Y);
+        /// <summary>
+        /// Get the Euclidean distance between two points in 2D space
+        /// </summary>
+        /// <remarks><see href="https://en.wikipedia.org/wiki/Euclidean_distance"/></remarks>
+        /// <returns>The Euclidean distance between two points</returns>
+        public static double EuclideanDistance2D(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        /// <summary>
+        /// Get the Euclidean distance between two points in 3D space
+        /// </summary>
+        /// <remarks><see href="https://en.wikipedia.org/wiki/Euclidean_distance"/></remarks>
+        /// <returns>The Euclidean distance between two points</returns>
+        public static double EuclideanDistance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+        }
+
+        #endregion
+    }
+}
